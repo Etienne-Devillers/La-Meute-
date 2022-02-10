283eaf73b8f5cae6188da932989b0618c68eee79
Etienne-Devillers <devillers.etienne80@gmail.com>
2022-02-10 14:48:59 +0100

    sidebar terminé

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,21 @@ window.addEventListener('scroll', () => {
     navBarAttached()
 })
 
+
+document.querySelector('.burgerMenu').addEventListener('click', () => {
+    console.log('bonjour')
+    document.querySelector('.sideNavBarContainer').classList.add('active');
+    document.querySelector('.sideNavBar').classList.add('sideNavBarActive');
+})
+
+document.querySelector('.sideNavBarContainer').addEventListener('click', () => {
+    document.querySelector('.sideNavBar').classList.remove('sideNavBarActive');
+    
+    document.querySelector('.sideNavBarContainer').classList.remove('active');
+
+})
+
+
+document.querySelector('.sideNavBarContainer').addEventListener('click', () => {
+    document.querySelector('.sideNavBarContainer').classList.remove('active');
+})
