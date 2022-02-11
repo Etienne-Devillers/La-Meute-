7e53c4e7d7f35afdb8211680b17e5ba0254b2277
Etienne-Devillers <devillers.etienne80@gmail.com>
2022-02-11 16:27:25 +0100

    commentSection commencé

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,9 +1,30 @@
-function navBarAttached()  {
+// Fonction qui s'occupe de la navbar qui change au scroll
+function navBarAttached() {
     if (document.documentElement.scrollTop > 80) {
         document.querySelector('.navBar').classList.add('fixed')
         document.querySelector('.logoNavBar').classList.add('logoNavBarFixed')
     } else {
         document.querySelector('.navBar').classList.remove('fixed')
         document.querySelector('.logoNavBar').classList.remove('logoNavBarFixed')
+    }
+}
+
+//Fonction qui calcul la position de la section et qui affiche le texte en fonction.
+
+function appearText() {
+    if (functionToggler === 'stopfunction') {    //création d'un if pour empecher les executions suivantes de la fonction
+        return;
+    } else {
+        var gameCollectionOffset = document.querySelector('.gameCollectionSection').getBoundingClientRect();
+    }
+    if (gameCollectionOffset.top < window.screen.availHeight * 0.40) {
+        let delay = 0;
+        playLearnImprove.forEach(element => {
+
+            element.style.animation = 'appearingText 400ms ease forwards';
+            element.style.animationDelay = delay + 'ms';
+            delay += 200;
+        });
+        functionToggler = 'stopfunction'  // on change la valeur du toggler pour empecher les prochaines executions de la fonction
     }
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
+const playLearnImprove = document.querySelectorAll('span h6')
+let functionToggler = 'go'
+// Partie NAvBar 
+
+navBarAttached()  // On lance ces fonctions à l'initialisation au cas ou le visiteur n'est pas en haut de page
+appearText()
+
+
+
 window.addEventListener('scroll', () => {
     navBarAttached()
 })
-
-
 document.querySelector('.burgerMenu').addEventListener('click', () => {
     console.log('bonjour')
     document.querySelector('.sideNavBarContainer').classList.add('active');
@@ -11,12 +18,22 @@ document.querySelector('.burgerMenu').addEventListener('click', () => {
 
 document.querySelector('.sideNavBarContainer').addEventListener('click', () => {
     document.querySelector('.sideNavBar').classList.remove('sideNavBarActive');
-    
+
     document.querySelector('.sideNavBarContainer').classList.remove('active');
 
 })
-
 
 document.querySelector('.sideNavBarContainer').addEventListener('click', () => {
     document.querySelector('.sideNavBarContainer').classList.remove('active');
 })
+
+
+// Apparition du texte "jouez, apprennez, progressez"
+
+
+
+
+
+window.addEventListener('scroll', () => {
+    appearText()
+});
