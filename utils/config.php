86eddd9c6d101c38ab773f9b668565617ae63ea7
Etienne-Devillers <devillers.etienne80@gmail.com>
2022-05-05 16:21:49 +0200

    ..

--- utils/config.php
+++ utils/config.php
@@ -4,7 +4,7 @@ define('REGEX_USERNAME', "^[A-Za-z-éèêëàâäôöûüç'_0-9 ]{3,20}+$");
 define('REGEX_PASSWORD', ".*^(?=.{8,30})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$"); //Au moins une majuscule, une minuscule, un chiffre et un caractere special.
 
 
-test
+
 define('DSN', 'mysql:host=localhost;dbname=lameute;charset=utf8');
 define('USERDB', 'meulouSuperAdmin');
 define('PWD', '2wGmoyhD0.sMDST!');
